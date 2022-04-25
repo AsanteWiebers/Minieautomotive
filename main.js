9d35e905d30c36a0d82022743c3373e293bcb61f
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-04-25 17:11:26 +0200

    scroll reveal

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ function menuOpen(){
     }
 }
 console.log(hamburgerMenu);
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal('.logo', { delay: 50 });
