114b7bbbc7ff36d8e33105572c31256734bc1951
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-05-01 19:31:29 +0200

    mulitple changes

--- main.js
+++ main.js
@@ -12,3 +12,50 @@ function menuOpen(){
 }
 
 
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+
+ScrollReveal().reveal('.logo', {
+    delay: 500
+});
+ScrollReveal().reveal('.card', {
+    delay: 600,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.hero_title', {
+    delay: 600,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.hero_text', {
+    delay: 600,
+    origin: 'right'
+});
+
+ScrollReveal().reveal('.car_logo', {
+    delay: 600,
+    origin: 'bottom',
+    interval: 200
+});
+
+ScrollReveal().reveal('.cta_intro_title', {
+    delay: 600,
+    origin: 'bottom'
+});
+
+ScrollReveal().reveal('.cta_heading', {
+    delay: 700,
+    origin: 'right'
+});
+
+ScrollReveal().reveal('.contact_heading', {
+    delay: 700,
+    origin: 'right'
+});
+
